db137280544970a8aad01e4d329039774fa47436
mrtkyz <mrtkyz@hotmail.com>
2017-05-17 23:02:28 +0300

    asdf

---
+++ MrtkyzBlog/Database.Deployment/Program.cs
@@ -0,0 +1,34 @@
+﻿using DbUp;
+using System;
+using System.Reflection;
+
+namespace Database.Deployment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "";
+
+            var upgrader = DeployChanges.To
+                .SqlDatabase(connectionString)
+                .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                .LogToConsole().Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("DB Deploy Successfull !");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+            }
+        }
+    }
+}
